a38cc8d5393ec6131d738cba288e47a5ca1d6c93
Mora096 <martigonzalezachaval@gmail.com>
2023-06-02 17:26:58 -0300

    first commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,15 @@
+import styles from "../styles/spinner.css"
+
+
+
+function Spinner() {
+  return (
+<div className="spinner">
+  <div className="bounce1"></div>
+  <div className="bounce2"></div>
+  <div className="bounce3"></div>
+</div>
+  )
+}
+
+export default Spinner
